5f6c50f6a8625e7f0dd9e8d85b0cd3e3e43c636d
Susan Pokharel <pokharelj120@gmail.com>
2025-03-04 09:44:36 +0545

    Initial setup

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_API_Creation.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    
+}
